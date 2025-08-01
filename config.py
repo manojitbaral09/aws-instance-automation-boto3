6a30172715c68e986c54d4e860c0504d3ae8f937
Manojit Baral <manojitbaral@gmail.com>
2025-08-01 15:20:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
-
+AWS_REGION = "us-east-1"
+AMI_ID = "ami-0c02fb55956c7d316"  # Amazon Linux 2
+INSTANCE_TYPE = "t2.micro"
+KEY_PAIR_NAME = "boto3-keypair"
+SECURITY_GROUP_NAME = "boto3-sec-group"
+TAG_NAME = "Boto3CreatedInstance"
